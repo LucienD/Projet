ad69539d4a6b507698d1ddf1869e736672eb219c
lucien <luciendartois@gmail.com>
2016-02-18 21:05:43 +0100

    Update

--- client/js/app.js
+++ client/js/app.js
@@ -64,24 +64,13 @@ Portfolio.controller('mainController', function ($scope, $rootScope) {
     $rootScope.page = 'home';
 
     $scope.contents = [
-        {name: 'game', titleOver: "title = 'Game'", link: '#/game', url: 'templates/content/homepage/elmtGame.svg'},
-        {name: 'presentation', titleOver: "title = 'Presentation'", link: '#/presentation', url: 'templates/content/homepage/elmtPresentation.svg'},
-        {name: 'village', titleOver: "title = 'Village'", link: '#/village', url: 'templates/content/homepage/elmtVillage.svg'},
-        {name: 'contact', titleOver: "title = 'Contact'", link: '#/contact', url: 'templates/content/homepage/elmtContact.svg'}
+        {name: 'Game', titleOver: "title = 'Game'", link: '#/game', url: 'templates/content/homepage/elmtGame.svg'},
+        {name: 'Presentation', titleOver: "title = 'Presentation'", link: '#/presentation', url: 'templates/content/homepage/elmtPresentation.svg'},
+        {name: 'Village', titleOver: "title = 'Village'", link: '#/village', url: 'templates/content/homepage/elmtVillage.svg'},
+        {name: 'Contact', titleOver: "title = 'Contact'", link: '#/contact', url: 'templates/content/homepage/elmtContact.svg'}
     ];
     $scope.titleOver = function (content, title) {
-        if (content.name === 'game') {
-            $scope.title = 'Game';
-        }
-        if (content.name === 'presentation') {
-            $scope.title = 'Presentation';
-        }
-        if (content.name === 'village') {
-            $scope.title = 'Projets';
-        }
-        if (content.name === 'contact') {
-            $scope.title = 'Contact';
-        }
+            $scope.title = content.name;
     };
     $scope.titleLeave = function (title) {
         $scope.title = 'Bienvenue';
@@ -147,60 +136,6 @@ Portfolio.controller('ElementsContactCtrl', function ($scope, $rootScope, Title)
     $scope.title = Title;
     $scope.title = 'Contact';
 });
-//--------------------------------------------------------------------- Village des Controller Page Product
-//------------------------------------------------------------------
-
-
-//Portfolio.controller('ElementsPaperCtrl', function ($scope, $rootScope) {
-//    $rootScope.isActive = false;
-//    $scope.pageClass = 'page-product content-paper';
-//    $rootScope.page = 'product paper';
-//    $scope.imgHeaders = [
-//        {url: 'templates/product/img/headpaper.png', name: 'paper'}
-//    ];
-//    $scope.titleProject = "Projet Paper";
-//    $scope.resume = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-//    $scope.icos = [
-//        {url: 'templates/product/ico/css3-ico.svg'},
-//        {url: 'templates/product/ico/ai-ico.svg'},
-//        {url: 'templates/product/ico/html-ico.svg'}
-//    ];
-//    $scope.describes = [
-//        {name: 'step1', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"},
-//        {name: 'step2', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"},
-//        {name: 'step3', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"}
-//    ];
-//    $scope.imgContents = [
-//        {url: 'templates/product/img/paper-swift.png', name: 'paper-exemple'}
-//    ];
-//});
-//
-//
-//
-//Portfolio.controller('ElementsDataCtrl', function ($scope, $rootScope) {
-//    $rootScope.isActive = false;
-//    $scope.pageClass = 'page-product content-data';
-//    $rootScope.page = 'product data';
-//    $scope.imgHeaders = [
-//        {url: 'templates/product/img/headdata.png', name: 'data-visualisation'}
-//    ];
-//    $scope.titleProject = "Projet Data Visualisation";
-//    $scope.resume = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-//    $scope.icos = [
-//        {url: 'templates/product/ico/css3-ico.svg'},
-//        {url: 'templates/product/ico/js-ico.svg'},
-//        {url: 'templates/product/ico/html-ico.svg'}
-//    ];
-//    $scope.describes = [
-//        {name: 'step1', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"},
-//        {name: 'step2', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"},
-//        {name: 'step3', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"}
-//    ];
-//    $scope.imgContents = [
-//        {url: 'templates/product/img/data.jpg', name: 'data-exemple'}
-//    ];
-//});
-
 
 //-------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------- ELEMENTS PAGE PRESENTATION
@@ -211,8 +146,8 @@ Portfolio.controller('ElementsMeCtrl', function ($scope, $rootScope, Entourage, 
     $rootScope.page = 'elm-presentation me';
 
     $scope.resumes = [
-        {text: "Je me présente, Lucien Deleu, développeur web. Licencié en Communication en 2014, J'ai ensuite suivis un Master en Création numérique pour me spécialiser dans le développement Web, particulièrement l'aspect graphique tout en restant polyvalent avec un intérêt fort pour le développement en back end. Je crée principalement des sites via Wordpress,"},
-        {text: "création de thèmes personnalisables grâce à des plugins customisés.Je travail également sur Angularjs, un Framework permettant de faire des application web. Concernant la continuité de mon apprentissage, la prochaine étape est NodeJs en complément d'Angularjs... "}
+        {text: "Je me présente, Lucien Deleu, développeur web. Licencié en Communication en 2014, J'ai ensuite suivis un Master en Création numérique pour me spécialiser dans le développement Web, particulièrement l'aspect graphique tout en restant polyvalent avec un intérêt fort pour le développement en back end."},
+        {text: "Je crée principalement des sites via Wordpress, création de thèmes personnalisables grâce à des plugins customisés.Je travail également sur Angularjs, un Framework permettant de faire des application web. Concernant la continuité de mon apprentissage, la prochaine étape est NodeJs en complément d'Angularjs... "}
     ];
 
     $scope.json = {
@@ -236,8 +171,9 @@ Portfolio.controller('ElementsMeCtrl', function ($scope, $rootScope, Entourage, 
     $scope.legendGraph = false;
     $scope.optionsGraph = {
         segmentShowStroke: false,
-        scaleShowLabelBackdrop: true,
-        scaleShowLine: false
+        scaleShowLabelBackdrop: false,
+        scaleShowLine: false,
+        scaleFontColor: "transparent"
     };
 
     $scope.copains = Entourage.getIconEntourage();
